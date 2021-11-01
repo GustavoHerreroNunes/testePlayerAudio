6d7d0b02bae73e446981914c8ab050b43593e171
GustavoHerreroNunes <ghnunes2010@gmail.com>
2021-11-01 17:20:10 -0300

    Add método para alterar estado do player e alterando setSound

--- www/js/player.js
+++ www/js/player.js
@@ -11,7 +11,8 @@ var player = {
         volume: 0.0,
         ready: false,
         state: "",
-        duration: "",
+        duration: 0.0,
+        currentTime: 0.0
     },
 
     //Construtor do app
@@ -23,16 +24,25 @@ var player = {
     //Quando todas as estruturas cordova tiverem inicializado
     onDeviceReady: function(){
         console.info("Device Ready");
-        player.initializeSound();
-        // player.playSong();
+        document.getElementById("btnPlay").addEventListener("click", player.setPlayerState);
+        
+        //Botão para iniciar a tocar o áudio pela primeira vez
+        document.getElementById("btnStartReading").addEventListener("click", () => {
+            player.setPlayerState();
+            document.getElementById("btnStartReading").style.display = "none";
+        });
+        player.setSound();
+        /* 2ª opção: iniciar áudio automaticamente 
+        player.setPlayerState();
+        */
     },
 
-    //Definindo parâmetros para o áudio a ser tocado
-    initializeSound(){
+    //Método para definir os parâmetros para o áudio a ser tocado
+    setSound: function(){
         player.sound.name = "Prism";
         player.sound.src = "https://firebasestorage.googleapis.com/v0/b/testestreaming-9a6ba.appspot.com/o/sounds%2FOur_Champion.mp3?alt=media&token=73c25e33-d875-43ac-b9cb-69f612097a24";
-        player.sound.volume = 100.0;
-
+        player.sound.volume = 0.5;
+        
         //Enviando a url do áudio para o player no HTML
         player.element.children[0].src = player.sound.src;
         player.element.load();
@@ -41,10 +51,39 @@ var player = {
         do{
             player.sound.ready = (player.element.readyState === 4);
         }while(player.sound.ready);
+        
+        //Definindo o volume do áudio no player
+        player.element.volume = player.sound.volume;
 
         //Definindo 'state' e 'duration' de 'sound'
-        player.sound.duration = '';
+        player.sound.state = "paused";
+        player.sound.duration = player.element.duration;
+
+        //Habilitando botão para iniciar leitura
+        document.getElementById("btnStartReading").disabled = false;
     },
+
+    //Método para alterar o estado do player (iniciado ou pausado | played or paused)
+    setPlayerState: function(){
+        //Verificando estado atual do player
+        switch(player.sound.state){
+
+            case "played"://Iniciado
+                player.element.pause();
+                player.sound.state = "paused";
+                break;
+
+            case "paused"://Pausado
+                player.element.play();
+                player.sound.state = "played";
+                break;
+
+            default://Estado não conhecido
+                console.log("Estado não conhecido: " + player.sound.state);
+                break;
+        }
+    },
+
 }
 
-player.onDeviceReady();
+player.initialize();
